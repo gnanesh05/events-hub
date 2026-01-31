29e5d6e3957de0d006ac73176705dab8c85cc672
gnaneshmk <gnaneshwer.mk@hingehealth.com>
2026-01-31 15:16:35 +0530

    initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,51 @@
+export const events = [
+  {
+    title: "React Summit 2024",
+    image: "/images/event1.png",
+    slug: "react-summit-2024",
+    location: "Amsterdam, Netherlands",
+    date: "2024-06-14",
+    time: "09:00 AM"
+  },
+  {
+    title: "Next.js Conf 2024",
+    image: "/images/event2.png",
+    slug: "nextjs-conf-2024",
+    location: "San Francisco, CA",
+    date: "2024-10-15",
+    time: "10:00 AM"
+  },
+  {
+    title: "JSConf EU 2024",
+    image: "/images/event3.png",
+    slug: "jsconf-eu-2024",
+    location: "Berlin, Germany",
+    date: "2024-09-05",
+    time: "08:30 AM"
+  },
+  {
+    title: "DevOps World 2024",
+    image: "/images/event4.png",
+    slug: "devops-world-2024",
+    location: "Austin, TX",
+    date: "2024-11-12",
+    time: "09:00 AM"
+  },
+  {
+    title: "AI & Machine Learning Summit",
+    image: "/images/event5.png",
+    slug: "ai-ml-summit-2024",
+    location: "London, UK",
+    date: "2024-08-20",
+    time: "10:00 AM"
+  },
+  {
+    title: "Web3 Hackathon 2024",
+    image: "/images/event6.png",
+    slug: "web3-hackathon-2024",
+    location: "Singapore",
+    date: "2024-07-25",
+    time: "08:00 AM"
+  }
+]
+
